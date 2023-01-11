384bea86dce14fb3d7829d3ad3826479f50cdcee
Felix <felix@iron59.co.uk>
2023-01-11 12:39:29 +0000

    update stats

--- src/CxAdmin/api/cxStatistics.py
+++ src/CxAdmin/api/cxStatistics.py
@@ -7,14 +7,18 @@ class CxStatistics(CxItem):
     def getRealtimeStatistics(self) -> Any:
         raise NotImplementedError()
 
-    def getInteractions(self, between: tuple[datetime, datetime]) -> Any:
+    def getInteractions(
+        self, between: tuple[datetime, datetime]
+    ) -> list[dict[str, Any]]:
         params: str = f"?start={between[0]}&end={between[1]}&limit=1000"
         responsesJsons: list[dict[str, Any]] = []
         response = self._httpClient.get(f"{self._path}/interactions{params}")
-        thisResponseJson = response.json()
-        responsesJsons.extend(thisResponseJson["results"])
-        while thisResponseJson["total"] >= 1000:
+        responseResults = response.json()["results"]["interactions"]
+        responsesJsons.extend(responseResults)
+        while len(responseResults) >= 1000:
             params = f"?start={between[0]}&end={between[1]}&limit=1000&offset={len(responsesJsons)}"
             response = self._httpClient.get(f"{self._path}/interactions{params}")
-            thisResponseJson = response.json()
-            responsesJsons.extend(thisResponseJson["results"])
+            responseResults = response.json()
+            responsesJsons.extend(responseResults["results"])
+
+        return responsesJsons
